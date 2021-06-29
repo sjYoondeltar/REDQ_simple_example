254d1533b6c65f65c24a97cf687da2219d9889e7
sjYoondeltar <dbsdns@naver.com>
2021-06-29 13:32:51 +0900

    modify the results img

--- example/plot_results.py
+++ example/plot_results.py
@@ -34,17 +34,17 @@ def main_plot():
     
     ax1.set_xlabel("episode")
     
-    ax1.set_xlim([0, 200])
+    ax1.set_xlim([0, 50])
 
     ax1.grid()
 
     ax1.legend()
 
-    # plt.show()
+    plt.show()
 
-    img_path = os.path.join(os.getcwd(), 'example', 'img')
+    # img_path = os.path.join(os.getcwd(), 'example', 'img')
 
-    plt.savefig(os.path.join(img_path, "comparison.png"))
+    # plt.savefig(os.path.join(img_path, "comparison.png"))
 
 
 if __name__ == '__main__':
