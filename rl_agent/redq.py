e8e98f30b5251a4d9e7d57e04ec9cb7043987ef7
sjYoondeltar <dbsdns@naver.com>
2021-09-04 00:24:19 +0900

    modify the redq actor loss

--- rl_agent/redq.py
+++ rl_agent/redq.py
@@ -369,23 +369,25 @@ class REDQAgent(object):
             # update actor
             policy, log_policy = self.eval_action(states)
             
-            q_loss = 0
+            # q_loss = 0
 
-            for i in range(self.N):
+            # for i in range(self.N):
 
-                q_value1 = self.critic_list[i](torch.Tensor(states).to(self.device), policy)
+            #     q_value1 = self.critic_list[i](torch.Tensor(states).to(self.device), policy)
 
-                q_loss = q_loss + q_value1
+            #     q_loss = q_loss + q_value1
 
-            q_loss = q_loss/self.N
+            # q_loss = q_loss/self.N
 
-            # idx = np.random.choice(self.N, self.M, replace=False)
+            idx = np.random.choice(self.N, self.M, replace=False)
             
-            # q_value1 = self.critic_list[idx[0]](torch.Tensor(states).to(self.device), policy)
+            q_value1 = self.critic_list[idx[0]](torch.Tensor(states).to(self.device), policy)
 
-            # q_value2 = self.critic_list[idx[1]](torch.Tensor(states).to(self.device), policy)
+            q_value2 = self.critic_list[idx[1]](torch.Tensor(states).to(self.device), policy)
 
             # q_loss = torch.min(q_value1, q_value2).squeeze(1)
+
+            q_loss = (q_value1 + q_value2)/2
 
             if self.train_alpha:
                 actor_loss = ((self.alpha.to(self.device) * log_policy) - q_loss).mean() 
